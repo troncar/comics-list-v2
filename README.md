0cd4124e6c2d1932e0def3dd7518ac40844b2bfe
Nuria Tronco <nuria.tronco@gmail.com>
2020-05-02 13:32:55 -0500

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-Comics list made with React and Api https://api.shortboxed.com/
-
-
---
+++ precache-manifest.904bf8fa316f51beec1f7593c700f32c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e6a1f946b28a75d42886ea7f256b3e6",
+    "url": "/comics-list-v2/index.html"
+  },
+  {
+    "revision": "e33c413baeda35815d58",
+    "url": "/comics-list-v2/static/css/main.f8eb9309.chunk.css"
+  },
+  {
+    "revision": "6094dfa6f4a494c72d66",
+    "url": "/comics-list-v2/static/js/2.2c489a40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/comics-list-v2/static/js/2.2c489a40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e33c413baeda35815d58",
+    "url": "/comics-list-v2/static/js/main.b5aaed12.chunk.js"
+  },
+  {
+    "revision": "2b33fe0f8168c4cf904d",
+    "url": "/comics-list-v2/static/js/runtime-main.64a03a85.js"
+  }
+]);
